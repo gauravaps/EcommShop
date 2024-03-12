c60afaf52f41bcc9ba00661c8bcc4bf0e13f645b
gauravaps <gauravapsout@look.com>
2024-03-12 12:47:52 +0530

    remove dot

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
-import {Outlet} from 'react-router-dom';
-import Footer from './components/Footer';
+import {Outlet} from 'react-router-dom'
+import Footer from './components/Footer'
 
 
 const App = () => {
